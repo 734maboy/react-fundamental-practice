3fdb28d808bc5e68958acd325c93ade5a1e6a186
734maboy <supamem74@gmail.com>
2022-07-10 11:38:34 +0300

    create-post-list-component
    working with array of items to show them through array.map method in `Post List` Component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import React, { useState } from 'react'
 import './styles/app.css'
 import PostItem from './components/PostItem'
+import PostList from './components/PostList'
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'С#', body: 'Description' },
+    { id: 3, title: 'Java', body: 'Description' },
+    { id: 4, title: 'Ruby', body: 'Description' },
+    { id: 5, title: 'C/C++', body: 'Description' },
+    { id: 6, title: 'Python', body: 'Description' },
+    { id: 7, title: 'PHP', body: 'Description' },
+
+  ]);
+
 
   return (
     <div className="App">
-      <PostItem
-        post={{ id: 1, title: 'Javascript', body: 'Description' }}
-      />
-      <PostItem
-        post={{ id: 2, title: 'С#', body: 'Description' }}
-      />
-      <PostItem
-        post={{ id: 3, title: 'Java', body: 'Description' }}
-      />
-      <PostItem
-        post={{ id: 4, title: 'Ruby', body: 'Description' }}
-      />
-      <PostItem
-        post={{ id: 5, title: 'C/C++', body: 'Description' }}
-      />
-      <PostItem
-        post={{ id: 6, title: 'Python', body: 'Description' }}
-      />
-      <PostItem
-        post={{ id: 7, title: 'PHP', body: 'Description' }}
-      />
+      <PostList posts={posts} title={"Список постов"}/>
     </div>
   );
 }
