34ced346d2ee829609231ab3afe26209d4ec05ef
734maboy <supamem74@gmail.com>
2022-07-10 11:02:18 +0300

    create-counter-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Counter from './components/Counter'
 
 function App() {
+
   return (
     <div className="App">
-
+      <Counter/>
     </div>
   );
 }
