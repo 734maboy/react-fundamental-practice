314295d5737efb9e5bb4f8746affc5fe12658e74
734maboy <supamem74@gmail.com>
2022-07-10 11:25:18 +0300

    create-post-item-component
    Work with some styles and props object in component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { useState } from 'react'
-import Counter from './components/Counter'
-import ClassCounter from './components/ClassCounter'
+import './styles/app.css'
+import PostItem from './components/PostItem'
 
 function App() {
 
   return (
     <div className="App">
-      <ClassCounter/>
+      <PostItem
+        post={{ id: 1, title: 'Javascript', body: 'Description' }}
+      />
+      <PostItem
+        post={{ id: 2, title: 'С#', body: 'Description' }}
+      />
+      <PostItem
+        post={{ id: 3, title: 'Java', body: 'Description' }}
+      />
+      <PostItem
+        post={{ id: 4, title: 'Ruby', body: 'Description' }}
+      />
+      <PostItem
+        post={{ id: 5, title: 'C/C++', body: 'Description' }}
+      />
+      <PostItem
+        post={{ id: 6, title: 'Python', body: 'Description' }}
+      />
+      <PostItem
+        post={{ id: 7, title: 'PHP', body: 'Description' }}
+      />
     </div>
   );
 }
