5cd036814eaab870b833c59c41f6cb03af092c9e
734maboy <supamem74@gmail.com>
2022-07-28 22:54:20 +0300

    feature-post-item-anination
    + Post item animated through react transition group

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PostItem from './PostItem'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 const PostList = ({ remove, posts, title }) => {
 
@@ -16,9 +17,17 @@ const PostList = ({ remove, posts, title }) => {
 			<h1 style={{
 				textAlign: 'center',
 			}}> {title} </h1>
-			{posts.map( (post, index) =>
-				<PostItem remove={remove} post={post} number={index + 1} key={`${post.id}_${post.title}_${post.body}`}/>
-			)}
+			<TransitionGroup>
+				{posts.map( (post, index) =>
+					<CSSTransition
+						key={`${post.id}_${post.title}_${post.body}`}
+						timeout={500}
+						classNames="post"
+					>
+						<PostItem remove={remove} post={post} number={index + 1} />
+					</CSSTransition>
+				)}
+			</TransitionGroup>
 		</div>
 	)
 }
