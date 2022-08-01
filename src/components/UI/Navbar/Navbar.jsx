2d202805a9c082734aac604af262fbb332d6c117
734maboy <supamem74@gmail.com>
2022-08-01 19:54:15 +0300

    feature-using-context
    + Added AuthContext
    + Added the direct use of AuthContext in router and navbar

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import SeButton from '../button/SeButton'
+import { AuthContext } from '../../../context'
 
 const Navbar = () => {
+	const {isAuth, setIsAuth} = useContext(AuthContext);
+
+	const logout = (e) => {
+		setIsAuth(false);
+		localStorage.removeItem('auth');
+	}
+
 	return (
 		<div className={"navbar"}>
-			<div className="navbar__links">
-				<Link className={"navbar-item"} to="/about">About</Link>
-				<Link className={"navbar-item"} to="/posts">Posts</Link>
-				<Link className={"navbar-item"} to="/login">Login</Link>
-			</div>
+			{
+				isAuth ?
+				<SeButton onClick={logout}>
+					Выйти
+				</SeButton> : ''
+			}
+
+				{
+					isAuth
+					? <div className="navbar__links">
+							<Link className={"navbar-item"} to="/about">About</Link>
+							<Link className={"navbar-item"} to="/posts">Posts</Link>
+						</div>
+					:
+						<Link className={"navbar-item"} to="/login">Login</Link>
+				}
 		</div>
 	)
 }
